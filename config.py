69953bed5ba0ce88007950f7e55d6a58cbbb12f8
vampirekingop07 <naaztravels62@gmail.com>
2025-10-18 19:18:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vampirebotsntwrk/VAMPIRE-MUSIC",
+    "https://github.com/vampirekingop07/UNTOLD-REPO-OP",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
