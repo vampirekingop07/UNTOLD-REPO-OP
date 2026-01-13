c12b773179b3b5dc045991adbf99c92f81f40cf0
BADNAM XD <yadavxd746@gmail.com>
2026-01-13 14:53:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,9 +33,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
-API_KEY = getenv("API_KEY", '30DxNexGenBots9135f8')
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_5q9vyB7GPcuQv4hp5tSXH6TS6OQaq58Z') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
