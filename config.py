98a379fa397a2904aa79c659d1cfd2c3db8fd423
vampirekingop07 <naaztravels62@gmail.com>
2026-01-13 15:48:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -85,18 +85,25 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", https://files.catbox.moe/oivetw.jpg"
+    "START_IMG_URL",
+    "https://files.catbox.moe/oivetw.jpg"
 )
+
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/oivetw.jpg"
+    "PING_IMG_URL",
+    "https://files.catbox.moe/oivetw.jpg"
 )
+
 PLAYLIST_IMG_URL = "https://i.ibb.co/k3cw8XS/IMG-20250103-174108-968.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/oivetw.jpg"
+
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+
 STREAM_IMG_URL = "https://i.ibb.co/k3cw8XS/IMG-20250103-174108-968.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
